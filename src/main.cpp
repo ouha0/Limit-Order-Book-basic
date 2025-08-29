8681f96d0b873959f7d7040ef8b5dfe54f7f9e7a
Hai Ou <ouha019@gmail.com>
2025-08-29 17:12:55 +1000

    filled in main.cpp; IO reading to vector container, processing commands, measuring results

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include "lob/Command.h"
 #include <iostream>
 #include <vector>
-#include <chrono>
+#include <chrono> // use to measure efficiency
 
 
 /* Function that prints the tradelog */
@@ -28,12 +28,48 @@ void print_trades(const LimitOrderBook& lob) {
 
 
 /*  Main function that takes in trades */
-int main () {
+int main (int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "Not enough arguments; input missing" << std::endl;
+        return 1;
+    }
+    
+    /* Part 1: Store .txt input into vector container */
+    std::string filename = argv[1];
+    std::vector<Command> commands = Parser::parse_commands(filename);
+    if (commands.empty()) {
+        std::cerr << "commands not successfully read from txt file" << std::endl;
+        return 1;
+    }
+    std::cout << "Successfully parsed " << commands.size() << " commands." << std::endl;
 
+    /* Part 2: lob takes in orders. Start measuring speed of LOB here */
+    
+    /* Setup */
     LimitOrderBook lob;
     OrderId order_id_counter = 1;
 
+    std::cout << "Processing orders..." << std::endl;
+    
+    // auto start = std::chrono::high_resolution_clock::now();
 
+    /* Adding commands to LOB */
+    for (const auto& cmd : commands) {
+        if (cmd.type == CommandType::Add) {
+            lob.add_order({order_id_counter++, cmd.price, cmd.quantity, cmd.side});
+
+        } else if (cmd.type == CommandType::Cancel) {
+            lob.cancel_order(cmd.id);
+
+        }
+    }
+
+    // auto end = std::chrono::high_resolution_clock::now();
+
+    /* Part 3: Check behaviour and time */
+    // auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    // std::cout << "\n Processing took " << duration << " milliseconds" << std::endl;
+    // std::cout << "Total Trades: " << lob.get_trades().size() << std::endl;
 
     return 0;
 }
